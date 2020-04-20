8faa20fa04a4a88b9b5d8a86a5baa2b35780f731
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-04-20 12:43:23 -0500

    Add files via upload

--- MontyUtils.py
+++ MontyUtils.py
@@ -2,6 +2,7 @@ __author__="Hugo Liu <hugo@media.mit.edu>"
 __version__="2.0"
 import os,string,sys
 
+
 class MontyUtils:
 
     def __init__(self):
@@ -9,32 +10,34 @@ class MontyUtils:
 
     def find_file(self,filename):
 
-        if filename in os.listdir('.'):
-            return './'+filename
+        # if filename in os.listdir('.'):
+        #     return './'+filename
 
-        if 'MONTYLINGUA' in os.environ:
-            csplits=os.environ['MONTYLINGUA'].split(';')
-            csplits=[groupss.strip() for groupss in csplits]
+        # if 'MONTYLINGUA' in os.environ:
+        #     csplits=os.environ['MONTYLINGUA'].split(';')
+        #     csplits=[groupss.strip() for groupss in csplits]
 
-            for enabled_arr in csplits:
+        #     for enabled_arr in csplits:
 
-                try :
+        #         try :
 
-                    if filename in os.listdir(enabled_arr):
-                        return enabled_arr+'/'+filename
-                except :
-                    pass
+        #             if filename in os.listdir(enabled_arr):
+        #                 return enabled_arr+'/'+filename
+        #         except :
+        #             pass
 
-        if 'PATH' in os.environ:
-            csplits=os.environ['PATH'].split(';')
-            csplits=[groupss.strip() for groupss in csplits]
+        # if 'PATH' in os.environ:
+        #     csplits=os.environ['PATH'].split(';')
+        #     csplits=[groupss.strip() for groupss in csplits]
 
-            for enabled_arr in csplits:
+        #     for enabled_arr in csplits:
 
-                try :
+        #         try :
 
-                    if filename in os.listdir(enabled_arr):
-                        return enabled_arr+'/'+filename
-                except :
-                    pass
-        return ''
+        #             if filename in os.listdir(enabled_arr):
+        #                 return enabled_arr+'/'+filename
+        #         except :
+        #             pass
+        # return ''
+
+        return f"{os.path.dirname(__file__)}\\{filename}"
